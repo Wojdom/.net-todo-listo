d1b67bfb25a1798b73a43c053c19d4fb3599ec13
Dominik <dom.wojtczak@gmail.com>
2025-03-17 12:31:22 +0100

    Allow removal of tasks list

---
+++ ToDoList/Migrations/20250317112756_TasksList_CascadeDelete.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList.Migrations
+{
+    /// <inheritdoc />
+    public partial class TasksList_CascadeDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Todos_TasksLists_TasksListId",
+                table: "Todos");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Todos_TasksLists_TasksListId",
+                table: "Todos",
+                column: "TasksListId",
+                principalTable: "TasksLists",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Todos_TasksLists_TasksListId",
+                table: "Todos");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Todos_TasksLists_TasksListId",
+                table: "Todos",
+                column: "TasksListId",
+                principalTable: "TasksLists",
+                principalColumn: "Id");
+        }
+    }
+}
